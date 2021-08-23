7fb65cd6098c6c6de6a5d4b633ce4b7ee25d6174
Romain Regaud <furious_harry@hotmail.com>
2021-08-23 14:19:50 +0200

    evo 1

--- bogie/lib.py
+++ bogie/lib.py
@@ -5,38 +5,50 @@ import numpy as np
 
 def try_me():
     possibilite = ['pierre', 'feuille', 'ciseaux']
-
-    print('Bonjour, bienvenue dans pierre / feuille / ciseaux \n \
-          Vous allez vous confronter au maitre du jeu'                                                                                                                                                                  )
+    Users = input("Quel est votre nom ?")
+    print(f'Bonjour {Users}, bienvenue dans pierre / feuille / ciseaux \n \
+          Vous allez vous confronter au maitre du jeu'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            )
     input("")
-    print('prêt ?')
+    print('Etes vous prêt ?')
     input("")
-    print('quel est votre choix ? \n \
-           1 = pierre \n    2 = feuille \n 3 = ciseaux'                                                                                                                                                                     )
-    forma = False
-    while forma == False:
-        answer = input()
-        if answer in ['1','2','3']:
-            forma = True
-    game = np.random.randint(1,3)
-    if game == 1:
-        if answer == 1:
-            print('égalité')
-        elif answer == 3:
-            print('YOU ARE A LOSER !!!!')
-        else :
-            print('vous avez gagné mais à mon avis vous devez avoir triché')
-    elif game == 2:
-        if answer == 2:
-            print('égalité')
-        elif answer == 1:
-            print('YOU ARE A LOSER !!!!')
-        else:
-            print('vous avez gagné mais à mon avis vous devez avoir triché')
-    elif game == 3:
-        if answer == 3:
-            print('égalité')
-        elif answer == 2:
-            print('YOU ARE A LOSER !!!!')
-        else:
-            print('vous avez gagné mais à mon avis vous devez avoir triché')
+    bot, User, manche = 0, 0, 0
+    while User != 3 or bot !=3:
+        print(f'Manche N°{manche} \n quel est votre choix ? \n \
+            1 = pierre \n    2 = feuille \n 3 = ciseaux'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            )
+        forma = False
+        while forma == False:
+            answer = input()
+            if answer in ['1','2','3']:
+                forma = True
+        game = np.random.randint(1,3)
+        if game == 1:
+            if answer == 1:
+                print('égalité')
+            elif answer == 3:
+                bot += 1
+                print('YOU ARE A LOSER !!!!')
+            else :
+                User += 1
+                print('vous avez gagné mais à mon avis vous devez avoir triché')
+        elif game == 2:
+            if answer == 2:
+                print('égalité')
+            elif answer == 1:
+                bot += 1
+                print('YOU ARE A LOSER !!!!')
+            else:
+                User += 1
+                print('vous avez gagné mais à mon avis vous devez avoir triché')
+        elif game == 3:
+            if answer == 3:
+                print('égalité')
+            elif answer == 2:
+                bot += 1
+                print('YOU ARE A LOSER !!!!')
+            else:
+                User += 1
+                print('vous avez gagné mais à mon avis vous devez avoir triché')
+    if bot == 3:
+        print('YOU ARE SUCH A HUGE LOOOOooooOOOOSER !!!!')
+    else:
+        print('You should not win, that is a fake news!!')
